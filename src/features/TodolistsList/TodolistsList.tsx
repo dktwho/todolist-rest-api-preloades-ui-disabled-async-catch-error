f7635a0905523fe9c6891fc892709dad98b3883d
Alexander Korobov <dktwho@mail.ru>
2023-10-12 15:32:10 +0300

    add catch to addTaskTC

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -72,7 +72,7 @@ export const TodolistsList: React.FC = () => {
 
     return <>
         <Grid container style={{padding: '20px'}}>
-            <AddItemForm addItem={addTodolist} />
+            <AddItemForm addItem={addTodolist} disabled={todolists.length === 10} />
         </Grid>
         <Grid container spacing={3}>
             {
--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -96,7 +96,10 @@ export const addTaskTC = (title: string, todolistId: string) => (dispatch: Dispa
                 dispatch(setStatusAC('failed'))
             }
 
-        })
+        }).catch((e) => {
+        dispatch(setStatusAC('failed'))
+        dispatch(setErrorAC(e.message))
+    })
 }
 export const updateTaskTC = (taskId: string, domainModel: UpdateDomainTaskModelType, todolistId: string) =>
     (dispatch: Dispatch<ActionsType>, getState: () => AppRootStateType) => {
